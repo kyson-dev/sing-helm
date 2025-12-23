fe0666213dda77249ffe0c5785d60f09130354ad
kyson <zou.kyson@gmail.com>
2025-12-23 11:23:28 +0800

    refactor: simplify CLI version command output logging and update comment.

--- internal/adapter/cli/root.go
+++ internal/adapter/cli/root.go
@@ -7,7 +7,7 @@ import (
 )
 
 // RootCmd 是为了让 main 能访问，但实际不建议直接暴露全局变量
-// 这里演示“依赖注入”式的构建
+// 这里演示"依赖注入"式的构建
 func NewRootCommand() *cobra.Command {
 	var debug bool
 	cmd := &cobra.Command{
@@ -35,9 +35,7 @@ func newVersionCommand() *cobra.Command {
 		Use:   "version",
 		Short: "Show version information",
 		Run: func(cmd *cobra.Command, args []string) {
-			// 这里不直接 fmt.Println，而是用 logger，虽然有点大材小用，但保持一致
-			//fmt.Fprintln(cmd.OutOrStdout(), version.Info{})
-			logger.Get().Info("Version", "version", version.Info{})
+			logger.Get().Info((&version.Info{}).String())
 		},
 	}
 }
